c30b780070d0a5d0512669e22f5bcf588308b16c
GalacticTube <51178800+GalacticTube@users.noreply.github.com>
2020-03-20 17:23:07 +0100

    Create ban.js

---
+++ commands/moderation/ban.js
@@ -0,0 +1,20 @@
+module.exports = {
+name: "ban",
+aliases: ["ban", "of", "aliases"]
+category: "moderation",
+description: "Ban user from server"
+usage: "<input>",
+run: (client, message, args) => {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply('Sorry you do not have permission!');
+        let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(!member) return message.reply("Please mention a valid user");
+        if(!member.bannable) return message.channel.send("Sorry I cannot ban that person! Do they have a higher role?");
+
+        let reason = args.slice(1).join(' ');
+        if(!reason) reason = "No reason provided";
+
+        await member.ban(reason)
+            .catch(e => message.reply(`Sorry I couldn't ban them! Error: ${e}`));
+        message.reply(`:white_check_mark: User banned!`);
+ }
+}
